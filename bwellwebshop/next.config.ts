a96bb69b6ca14ac7e33a85602aa24b5c1d885b0d
Owentnt <120107905+Owentnt@users.noreply.github.com>
2025-10-29 13:32:32 +0100

    test

--- bwellwebshop/next.config.ts
+++ bwellwebshop/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {
+        root: "C:\\Users\\owenh\\OneDrive\\Bureaublad\\2PR\\Q7\\bwellwebshop\\bwellwebshop"
+    },
 };
 
 export default nextConfig;
